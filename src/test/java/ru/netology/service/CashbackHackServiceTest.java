c3762a8d0a4efb88e6c909ed3277efb9d3aded6e
YuliyaSi <pirozhok_s_kapustpo@mail.ru>
2022-10-27 22:09:46 +0300

    test junit-jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Test;
+
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
@@ -37,5 +39,40 @@ public class CashbackHackServiceTest {
         int expected = 999;
         int actual = service.remain(1001);
         org.junit.Assert.assertEquals(actual,expected);
+    }
+    @org.junit.jupiter.api.Test
+    public void remain1000jupiter (){
+
+        int expected = 1000;
+        int actual = service.remain(0);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void remain200Jupiter (){
+
+        int expected = 200;
+        int actual = service.remain(800);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void remainZeroJupiter(){
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void remain1Jupiter (){
+
+        int expected = 1;
+        int actual = service.remain(999);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void remain999Jupiter (){
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 }
