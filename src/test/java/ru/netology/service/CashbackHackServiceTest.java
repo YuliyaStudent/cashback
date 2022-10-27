d1825e3aba1f490cadc1a896ab20adc0f9f07212
YuliyaSi <pirozhok_s_kapustpo@mail.ru>
2022-10-27 21:17:27 +0300

    test junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,44 +1,41 @@
 package ru.netology.service;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
-
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain1000 (){
 
         int expected = 1000;
         int actual = service.remain(0);
-        Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(actual,expected);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain200 (){
 
         int expected = 200;
         int actual = service.remain(800);
-        Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(actual,expected);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remainZero(){
 
         int expected = 0;
         int actual = service.remain(1000);
-        Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(actual,expected);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain1 (){
 
         int expected = 1;
         int actual = service.remain(999);
-        Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(actual,expected);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain999 (){
 
         int expected = 999;
         int actual = service.remain(1001);
-        Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(actual,expected);
     }
 }
